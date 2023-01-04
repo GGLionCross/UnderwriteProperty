badb0d6c4863f79bf73f492beff39c194c1e772f
Lionel Quiambao <lionelquiambao@gmail.com>
2023-01-04 11:58:30 -0800

    Abstracted make code to global batch files

--- make.bat
+++ make.bat
@@ -3,14 +3,6 @@
 REM make.bat is designed to replicated make functionality or npm scripts
 
 IF %1 EQU build pyinstaller --clean underwrite-property.spec
-REM config.json is the configuration file our exe ACTUALLY uses
-REM keep-config.json overwrites config.json when building
-REM If making temp changes to keep-config, default-config is used to store default settings
-IF %1 EQU default-config COPY ".ignore\default-config.json" ".ignore\keep-config.json"
-IF %1 EQU env py -m venv .env
-IF %1 EQU env-activate .env\Scripts\activate
-if %1 EQU requirements pip freeze > requirements.txt
-if %1 EQU setup pip install -r requirements.txt
 
 REM Remind user of available functions
 IF %1 EQU --help (
@@ -18,10 +10,5 @@ IF %1 EQU --help (
   ECHO AVAILABLE ARGS
   ECHO --------------
   ECHO build
-  ECHO default-config
-  ECHO env
-  ECHO env-activate
-  ECHO requirements
-  ECHO setup
   ECHO --------------
 )
